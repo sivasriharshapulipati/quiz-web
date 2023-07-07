01442bc2a1ede4ab8cbd9b86a0c72debdeef6973
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-07-07 12:06:09 +0530

    ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import Quiz from "./Quiz";
-import {jsQuizz } from './constants';
-
+import Quiz from "./components/Quiz/Quiz";
+import { jsQuizz } from "./constants";
 
 function App() {
   return <Quiz questions={jsQuizz.questions} />;
 }
-
 
 export default App;
